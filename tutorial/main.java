bb5e5eb32a0b171e5b06778fb05f5fd5adfffe5f
awlamah19-design <awlamah19@gmail.com>
2026-06-09 07:40:31 +0000

    Student Class

---
+++ tutorial/main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] arges) {
+        // coding starts here;
+
+        Student s1 = new Student("Ali", 20, 3.8);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExsam();
+
+    }
+}
